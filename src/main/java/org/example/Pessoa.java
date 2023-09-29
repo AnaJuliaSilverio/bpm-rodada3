9667d523e26b816bf403e35a6fe63680ef263432
AnaJuliaSilverio <ana.silverio@zup.com.br>
2023-09-29 14:48:12 -0300

    ♻️ refactor: Retirando gets e sets não utilizados

--- src/main/java/org/example/Pessoa.java
+++ src/main/java/org/example/Pessoa.java
@@ -8,18 +8,6 @@ public class Pessoa {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public Pessoa() {
 
     }
